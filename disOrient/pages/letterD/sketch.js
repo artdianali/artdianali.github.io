48cc8e32f6b7b8454f098059c8d21cb5cb8da19f
artdianali <art.dianali@gmail.com>
2016-11-15 21:38:55 -0800

    Add files via upload

---
+++ disOrient/pages/letterD/sketch.js
@@ -0,0 +1,25 @@
+// Get a random element from an array using the random(Array) syntax
+var words = [ "dementia", "diaspora", "disOrient", "daughter", "death", "diana", "deliverance", "dialysis", "disAssociate", "dominate", "disappoint", "disLocate" ];
+var word
+var value = d;
+
+function setup() {
+  createCanvas(1800,1000);
+  textFont("Courier");
+  word=random(words);
+  textSize(random(54,200));
+}
+
+
+function draw(){
+  background(253,203,50);
+  text(word,300,500);
+  keyPressed();
+}
+
+function keyPressed() {
+  if (key === 'd') {
+    textSize(random(48,200));
+    word=random(words);
+  } 
+}
