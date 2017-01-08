a56b869adcdc933d724b3bf3fb2d60378be97003
artdianali <art.dianali@gmail.com>
2017-01-07 18:54:32 -0800

    Add files via upload

---
+++ disOrient/pages/shutUp/sketch.js
@@ -0,0 +1,28 @@
+function preload() {
+  sound1=loadSound('assets/PlacidoDomingo.mp3');
+  sound2=loadSound('assets/SimonGarfunkel.mp3');
+  sound3=loadSound('assets/12GirlsBand.mp3');
+  img = loadImage("assets/condor.jpg");
+}
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  sound1.play();
+  sound2.play();
+  sound3.play();
+  textFont("Courier");
+  textSize(random(54,80));
+}
+
+function draw(){
+  image(img, 0, 0, width, height);
+  text("Shut the fuck up.",width/2.2,height/1.2);
+  fill(253,203,50);
+  if (mouseIsPressed) {
+    window.open('../visitor/index.html')
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth,windowHeight);
+}
