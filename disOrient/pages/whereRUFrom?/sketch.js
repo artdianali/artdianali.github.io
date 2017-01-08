1230742953ecc0fbd4f7ead732fb25ada8e5afe1
artdianali <art.dianali@gmail.com>
2017-01-07 17:34:54 -0800

    Add files via upload

---
+++ disOrient/pages/whereRUFrom?/sketch.js
@@ -0,0 +1,21 @@
+
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  textFont("Courier");
+  textSize(random(54,100));
+}
+
+
+function draw(){
+  background(0,139,139);
+  text("Where are you from?",width/7,height/2);
+  fill(253,203,50);
+  if (mouseIsPressed) {
+    window.open('../shutUp.html')
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth,windowHeight);
+}
