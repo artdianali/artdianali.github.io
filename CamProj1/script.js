b4dd26a5288b5d926ea7597be170858db0610bef
artdianali <art.dianali@gmail.com>
2017-04-11 16:35:47 -0700

    Update script.js

--- CamProj1/script.js
+++ CamProj1/script.js
@@ -1,3 +1,4 @@
+//https://kylemcdonald.github.io/cv-examples/BackgroundSubtraction/sketch.js
 var capture;
 var image;
 
@@ -29,6 +30,30 @@ function draw() {
     var pixels = capture.pixels;
     var thresholdAmount = select('#thresholdAmount').value() * 255. / 100.;
 
+          var total = 0;
+      for(var y = 0; y < h; y++) {
+        for(var x = 0; x < w; x++) {
+          // another common type of background thresholding uses absolute difference, like this:
+          // var total = Math.abs(pixels[i+0] - backgroundPixels[i+0] > thresholdAmount) || ...
+          var rdiff = Math.abs(pixels[i+0] - backgroundPixels[i+0]) > thresholdAmount;
+          var gdiff = Math.abs(pixels[i+1] - backgroundPixels[i+1]) > thresholdAmount;
+          var bdiff = Math.abs(pixels[i+1] - backgroundPixels[i+1]) > thresholdAmount;
+          var anydiff = rdiff || gdiff || bdiff;
+          var output = 0;
+          if(anydiff) {
+            output = 255;
+            total++;
+          }
+          pixels[i++] = output;
+          pixels[i++] = output;
+          pixels[i++] = output;
+          i++; // skip alpha
+        }
+      }
+      var n = w * h;
+      var ratio = total / n;
+      select('#presence').elt.innerText = int(100 * ratio); 
+    
     /*
     if (thresholdType === 'bw') {
       var total = 0;
@@ -55,7 +80,7 @@ function draw() {
       var ratio = total / n;
       select('#presence').elt.innerText = int(100 * ratio); 
     
-    } else {*/
+    } else {
       for(var y = 0; y < h; y++) {
         for(var x = 0; x < w; x++) {
           pixels[i] = pixels[i] - backgroundPixels[i]; i++;
@@ -64,8 +89,9 @@ function draw() {
           i++; // skip alpha
         }
       }
-    //}
+    }
   }
+  */
   capture.updatePixels();
   
   image(capture, 0, 0, 640, 480);
