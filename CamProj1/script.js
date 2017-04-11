5c9d45f41683191f63a51ff4811579c8d145745e
artdianali <art.dianali@gmail.com>
2017-04-11 16:58:40 -0700

    Update script.js

--- CamProj1/script.js
+++ CamProj1/script.js
@@ -19,11 +19,8 @@ function resetBackground() {
 function draw() {
   background(255, 204, 0);
   // Displays the image at its actual size at point (0,0)
-  if (ratio > 0.5) {
-    tint(255,0,0);
-  } else {
-    noTint();
-  }
+  var distortion = map(ratio,0,1,0,255);
+  tint(distorion,0,0);
   image(img, 647, 0);
   backgroundSubtraction();
 }
