e7502b9020f3d0d3290df2981ff0de018c33e84e
artdianali <art.dianali@gmail.com>
2017-06-28 00:58:05 -0700

    Update script.js

--- CamProj1 2/script.js
+++ CamProj1 2/script.js
@@ -71,7 +71,7 @@ function backgroundSubtraction() {
 
   capture.updatePixels();
 
-  image(capture, 0, 0, windowWidth, windowHeight);
+  //image(capture, 0, 0, windowWidth, windowHeight);
 }
 
 function pixelate() {
