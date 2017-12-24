31cc684fb1f0564358ffd6b60ce938ba5ddda437
artdianali <art.dianali@gmail.com>
2017-12-24 15:10:18 -0800

    Update sketch.js

--- disOrient/pages/shutUp/sketch.js
+++ disOrient/pages/shutUp/sketch.js
@@ -17,7 +17,7 @@ function setup() {
 
 function draw(){
   image(img, 0, 0, width, height);
-  text("shut the f*ck up", width/2.2, height/1.2, width/10, height/10);
+  text("shut the f*ck up");
   fill(253,203,50);
   if (mouseIsPressed) {
     window.open('../visitor/index.html')
